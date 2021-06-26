98a88a2a8d22b027e1ab4cd5b95e9cffc2bc61b6
Ricardo Bravo <ricardobravo@outlook.com>
2021-06-25 21:49:15 -0500

    refactor: Task for post request

--- TheMovieManager/Model/Api/ApiClient.swift
+++ TheMovieManager/Model/Api/ApiClient.swift
@@ -169,4 +169,32 @@ class ApiClient {
         task.resume()
     }
     
+    class func taskForPOSTRequest<RequestType: Encodable, ResponseType: Decodable>(url: URL, body: RequestType, response: ResponseType.Type, completion: @escaping (ResponseType?, Error?) -> Void) {
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        urlRequest.httpBody = try! JSONEncoder().encode(body)
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+                return
+            }
+            do {
+                let response = try JSONDecoder().decode(ResponseType.self, from: data)
+                DispatchQueue.main.async {
+                    completion(response, nil)
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+            }
+        }
+        task.resume()
+    }
+    
 }
